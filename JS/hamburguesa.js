f7d0a00218f9b5f7304d6d3937d3be3e4fe4b75e
binuelo <o.corona161193@gmail.com>
2022-10-20 06:49:09 -0500

    funcionalidad menu con Js

---
+++ JS/hamburguesa.js
@@ -0,0 +1,9 @@
+function showContent() {
+  element = document.getElementById("menu-hamburguesa");
+  check = document.getElementById("check");
+  if (check.checked || e.target.checked(`${menu - hamburguesa} *`)) {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
---
+++ JS/index_dom.js
@@ -0,0 +1,7 @@
+import hamburgerMenu from "./dom/menu_hamburguesa.js";
+
+hamburgerMenu(
+  ".panel-btn",
+  ".panel",
+  ".menu a"
+); /*nombres de selectores que me hacen funcionar el BTN y el elemento que hace funcionar el btn*/
